b5f4360f7150dcbd1b47b35f39babbede586d7e9
sunjingwen21 <1335810159@qq.com>
2024-01-20 16:08:56 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="欢迎来到懒猫 遇到问题请联系运维部"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/route.js
@@ -0,0 +1,4 @@
+import info from './pages/info/info'
+export default [
+    {icon: <DesktopOutlined/>, title: '工作台', path: '/home', component: info},
+]
